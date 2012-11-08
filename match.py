e88d6835306b7ba4857bc2c85627e641667034bb
Matthew Lewis <matt@mplewis.com>
2012-11-08 17:27:05 -0600

    actually added the files this time

---
+++ match.py
@@ -0,0 +1,30 @@
+import random
+
+import yaml
+with open('people.yml', 'r') as peopleData:
+	peopleData = yaml.load(peopleData)
+
+people = sorted(peopleData.keys())
+	
+random.seed()
+noGifterYet = people[:]
+
+# {key : value} --> {gifter : recipient}
+naughtyOrNiceDict = {}
+
+while noGifterYet != []:
+	for person in people:
+		try:
+			possRecipients = list(set(noGifterYet).difference(peopleData[person]['willNotGive']))
+			if person in possRecipients:
+				possRecipients.remove(person)
+			recipient = random.choice(possRecipients)
+			noGifterYet.remove(recipient)
+			naughtyOrNiceDict[person] = recipient
+		except IndexError:
+			print 'Ran out of people. Restarting...'
+			noGifterYet = people[:]
+			naughtyOrNiceDict = {}
+
+for person in people:
+	print person, 'is giving a gift to', naughtyOrNiceDict[person]
